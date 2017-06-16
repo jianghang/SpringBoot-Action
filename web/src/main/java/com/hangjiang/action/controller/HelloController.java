6cb5354c54f7a6881546944b0982d2a582e50760
JiangHang <664019848@qq.com>
2017-06-16 21:44:38 +0800

    20170616

--- web/src/main/java/com/hangjiang/action/controller/HelloController.java
+++ web/src/main/java/com/hangjiang/action/controller/HelloController.java
@@ -1,7 +1,9 @@
 package com.hangjiang.action.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Created by jianghang on 2017/3/26.
@@ -12,5 +14,12 @@ public class HelloController {
     @RequestMapping("/")
     public String hello(){
         return "hello.html";
+    }
+
+    @RequestMapping(value = "/helloTymeleaf",method = RequestMethod.GET)
+    public String helloThymeleaf(Model model){
+        model.addAttribute("name","thymeleaf");
+
+        return "helloTymeleaf";
     }
 }
